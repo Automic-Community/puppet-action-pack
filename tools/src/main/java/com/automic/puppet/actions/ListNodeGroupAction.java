ca7698ce5573d21f0f784bddab81f4cfa439180c
Anurag Upadhyay <extanu@automic.com>
2016-10-06 14:13:34 +0530

    <PMARA-3575:PMARA-3663>Action fails when "*" is provided in Filter Field bug fix

--- tools/src/main/java/com/automic/puppet/actions/ListNodeGroupAction.java
+++ tools/src/main/java/com/automic/puppet/actions/ListNodeGroupAction.java
@@ -2,6 +2,8 @@ package com.automic.puppet.actions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import com.automic.puppet.actions.helper.NodeGroupInfo;
 import com.automic.puppet.actions.helper.TokenHandler;
@@ -16,68 +18,72 @@ import com.sun.jersey.api.client.WebResource;
  */
 public class ListNodeGroupAction extends AbstractHttpAction {
 
-	/**
-	 * Node Group Filter
-	 */
-	private String nodeGroupsFilter;
+    /**
+     * Node Group Filter
+     */
+    private String nodeGroupsFilter;
 
-	public ListNodeGroupAction() {
-		addOption("nodegroupfilter", false, "Node Group filter");
-	}
+    public ListNodeGroupAction() {
+        addOption("nodegroupfilter", false, "Node Group filter");
+    }
 
-	@Override
-	protected void executeSpecific() throws AutomicException {
-		prepareInputParameters();
-		WebResource webResClient = getClient();
+    @Override
+    protected void executeSpecific() throws AutomicException {
+        prepareInputParameters();
+        WebResource webResClient = getClient();
 
-		// generate auth token
-		TokenHandler tHandler = new TokenHandler(webResClient);
-		String authToken = tHandler.login(username);
-		try {
-			// Get list of classes
-			List<String> nodeGroups = new NodeGroupInfo(authToken, webResClient)
-					.getNodeGroups();
-			// process response
-			prepareOutput(nodeGroups);
-		} finally {
-			// destroy token
-			tHandler.logout(authToken);
-		}
-	}
+        // generate auth token
+        TokenHandler tHandler = new TokenHandler(webResClient);
+        String authToken = tHandler.login(username);
+        try {
+            // Get list of classes
+            List<String> nodeGroups = new NodeGroupInfo(authToken, webResClient).getNodeGroups();
+            // process response
+            prepareOutput(nodeGroups);
+        } finally {
+            // destroy token
+            tHandler.logout(authToken);
+        }
+    }
 
-	private void prepareInputParameters() throws AutomicException {
-		// Initialize node group filter
-		nodeGroupsFilter = getOptionValue("nodegroupfilter");
-	}
+    private void prepareInputParameters() throws AutomicException {
+        // Initialize node group filter
+        nodeGroupsFilter = getOptionValue("nodegroupfilter");
+    }
 
-	// print the list of node groups in AE vara UC4RB_PUP_NODE_GROUP_LIST in the
-	// job report
-	private void prepareOutput(List<String> nodeGroups) {
-		// write the node group details to job report
-		List<String> filterNodeGroups = new ArrayList<String>();
-		if (!nodeGroups.isEmpty()) {
-			if (CommonUtil.checkNotEmpty(nodeGroupsFilter)) {
-				for (String group : nodeGroups) {
-					if (group.matches(nodeGroupsFilter)) {
-						filterNodeGroups.add(group);
-					}
-				}
-			} else {
-				filterNodeGroups = nodeGroups;
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for (String nodeGroup : filterNodeGroups) {
-			sb.append(nodeGroup).append(",");
-		}
-		if (sb.length() > 1) {
-			sb.deleteCharAt(sb.length() - 1);
-		}
+    // print the list of node groups in AE vara UC4RB_PUP_NODE_GROUP_LIST in the
+    // job report
+    private void prepareOutput(List<String> nodeGroups) {
+        // write the node group details to job report
+        List<String> filterNodeGroups = new ArrayList<String>();
+        if (!nodeGroups.isEmpty()) {
+            if (CommonUtil.checkNotEmpty(nodeGroupsFilter)) {
+                Pattern pt = null;
+                try {
+                    pt = Pattern.compile(nodeGroupsFilter);
+                } catch (PatternSyntaxException pe) {
+                    pt = Pattern.compile(Pattern.quote(nodeGroupsFilter));
+                }
+                for (String group : nodeGroups) {
+                    if (pt.matcher(group).matches()) {
+                        filterNodeGroups.add(group);
+                    }
+                }
+            } else {
+                filterNodeGroups = nodeGroups;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String nodeGroup : filterNodeGroups) {
+            sb.append(nodeGroup).append(",");
+        }
+        if (sb.length() > 1) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
 
-		ConsoleWriter.writeln("UC4RB_PUP_NODE_GROUP_LIST::=" + sb.toString());
-		ConsoleWriter.writeln("UC4RB_PUP_NODE_GROUP_COUNT::="
-				+ filterNodeGroups.size());
+        ConsoleWriter.writeln("UC4RB_PUP_NODE_GROUP_LIST::=" + sb.toString());
+        ConsoleWriter.writeln("UC4RB_PUP_NODE_GROUP_COUNT::=" + filterNodeGroups.size());
 
-	}
+    }
 
 }
